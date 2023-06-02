9e2bd10beb5ac2083207367dfa688376a4766814
bin <dpqls0625@naver.com>
2023-06-02 23:49:26 +0900

    1.3.1 산술연산

---
+++ 1.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# 1.3.1 산술 연산
+
+1-2
+
+4*5
+
+7/5
+
+3**2
+
+
