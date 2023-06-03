f0efae1a2d33e571479b70ce7e9362ad4c528f9b
bin <dpqls0625@naver.com>
2023-06-03 23:03:09 +0900

    커밋

--- 1.py
+++ 1.py
@@ -23,4 +23,112 @@
 
 3**2
 
+# 1.3.2 자료형
+
+type(10)
+
+type(2.718)
+
+type("hello")
+
+# 1.3.3 변수
+
+x=10
+
+print(x)
+
+x=100
+
+print(x)
+
+y=3.14
+
+x*y
+
+# 1.3.4 리스트
+
+a=[1,2,3,4,5]
+
+print(a)
+
+len(a)
+
+a[0]
+
+a[4]
+
+a[4]=99
+
+print(a)
+
+a[0:2]
+
+a[1:]
+
+a[:3]
+
+a[:-1]
+
+a[:-2]
+
+# 1.3.5 딕셔너리
+
+me={'height':100}
+
+me['height']
+
+me['weight']=70
+
+print(me)
+
+# 1.3.6 bool
+
+hungry=True
+
+sleepy=False
+
+type(hungry)
+
+not hungry
+
+hungry and sleepy
+
+hungry or sleepy
+
+# 1.3.7 if문
+
+hungry=True
+
+if hungry:
+    print(("I'm hungry"))
+
+hungry=False
+
+if hungry:
+    print("I'm hungry")
+else:
+    print("I'm not hungry")
+    print("I'm sleepy")
+
+# 1.3.8 for문
+
+for i in[1,2,3]:
+    print(i)
+
+
+# 1.3.9 함수
+
+def hello():
+    print("Hello World!")
+
+
+hello()
+
+
+def hello(object):
+    print("Hello "+object+"!")
+
+
+hello("cat")
+
 
