2444cb839a9bfffe88dbc8daa81d81519fe72306
bin <dpqls0625@naver.com>
2023-06-04 20:17:27 +0900

    커밋

--- 1.py
+++ 1.py
@@ -132,3 +132,155 @@ def hello(object):
 hello("cat")
 
 
+# 1.4.1 파일로 저장하기
+
+# 1.4.2 클래스
+
+class 클래스 이름:
+    def __init__(self, 인수, ...): #생성자
+        ...
+    def 메서드 이름 1(self, 인수, ...): #메서드1
+        ...
+    def 메서드 이름 2(self, 인수, ...): #메서드2
+        ...
+
+
+class Man:
+    def __init__(self, name):
+        self.name=name
+        print("Initialized!")
+    def hello(self):
+        print("Hello "+self.name+"!")
+    def goodbye(self):
+        print("Good-bye "+self.name+"!")
+m=Man("예빈")
+m.hello()
+m.goodbye()
+
+# 1.5 넘파이
+
+# 1.5.1 넘파이 가져오기
+
+import numpy as np
+
+# 1.5.2 넘파이 배열 생성하기
+
+x=np.array([1.0,2.0,3.0])
+
+print(x)
+
+type(x)
+
+# 1.5.3 넘파이의 산술연산
+
+x=np.array([1.0,2.0,3.0])
+
+y=np.array([2.0,4.0,6.0])
+
+x+y
+
+x-y
+
+x*y
+
+x/y
+
+x=np.array([1.0,2.0,3.0])
+
+x/2.0
+
+# 1.5.4 넘파이의 N차원 배열
+
+A=np.array([[1,2],[3,4]])
+
+print(A)
+
+A.shape
+
+A.dtype
+
+B=np.array([[3,0],[0,6]])
+
+A+B
+
+A*B
+
+print(A)
+
+A*10
+
+# 1.5.5 브로드캐스트
+
+A=np.array([[1,2],[3,4]])
+
+B=np.array([[10,20]])
+
+A*B
+
+# 1.5.6 원소 접근
+
+X=np.array([[51,55],[14,19],[0,4]])
+
+print(X)
+
+X[0]
+
+X[0][1]
+
+for row in X:
+    print(row)
+
+for row in X:
+    for col in row:
+        print(col)
+
+X=X.flatten()
+
+print(X)
+
+X[np.array([0,2,4])]
+
+X>15
+
+X[X>15]
+
+# 1.6 matplotlib
+
+# 1.6.1 단순한 그래프 그리기
+
+import matplotlib.pyplot as plt
+
+x=np.arange(0,6,0.1)
+
+y=np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+# 1.6.2 pyplot의 기능
+
+x=np.arange(0,6,0.1)
+
+y1=np.sin(x)
+
+y2=np.cos(x)
+
+plt.plot(x,y1, label="sin")
+plt.plot(x,y2,linestyle="--",label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin&cos')
+plt.legend()
+plt.show()
+
+# 1.6.3 이미지 표시하기
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img=imread('vr과제4.png')
+
+plt.imshow(img)
+plt.show()
+
+
