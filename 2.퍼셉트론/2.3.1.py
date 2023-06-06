cbcffc345d71a1d2ed8ce1f0fada09bf2ba57b11
bin <dpqls0625@naver.com>
2023-06-06 22:41:38 +0900

    커밋

---
+++ 2.퍼셉트론/2.3.1.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def AND(x1,x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1 * w1 + x2 * w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+
+
+AND(0,0) #0을 출력
+
+AND(1,0) #0을 출력
+
+AND(0,1) #0을 출력
+
+AND(1,1) #0을 출력
