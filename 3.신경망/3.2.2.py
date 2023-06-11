44f4ebb5bdc4bee4868d13d2b249228d5b7af259
bin <dpqls0625@naver.com>
2023-06-11 16:25:19 +0900

    커밋

---
+++ 3.신경망/3.2.2.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+
+x=np.array([-1.0,1.0,2.0])
+
+x
+
+y=x>0
+
+y
+
+y=y.astype(np.int)
+
+y
+
+
