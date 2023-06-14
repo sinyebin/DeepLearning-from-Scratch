0aaa4eceeb3e9f9a22d3f99ad10b3891d118b20b
bin <dpqls0625@naver.com>
2023-06-14 21:48:07 +0900

    커밋

---
+++ 4.신경망학습/function_2.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def function_2(x):
+    return x[0]**2+x[1]**2
+
+
