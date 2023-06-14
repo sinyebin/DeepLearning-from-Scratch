6e1be6bd47dc38c6eadcbc1be47aa27c1e9ae49f
bin <dpqls0625@naver.com>
2023-06-14 21:25:45 +0900

    커밋

---
+++ 4.신경망학습/numerical_diff.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
