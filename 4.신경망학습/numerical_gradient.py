42db3a86f7529fa8ebc324fa2beef20986a1f649
bin <dpqls0625@naver.com>
2023-06-14 22:05:41 +0900

    커밋

---
+++ 4.신경망학습/numerical_gradient.py
@@ -0,0 +1,45 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val  # 값 복원
+
+    return grad
+
+
+import numpy as np
+import function_2
+
+numerical_gradient(function_2.function_2,np.array([3.0,4.0]))
+
+print(numerical_gradient(function_2.function_2, np.array([0.0, 2.0])))
+
+print(numerical_gradient(function_2.function_2, np.array([3.0, 0.0]))) 
+
+
