5d2ed346b83928c20b4246a583c7ecb3fd2e9c23
bin <dpqls0625@naver.com>
2023-06-18 23:01:47 +0900

    커밋

---
+++ 5.오차역전파법/layer_naive.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y                
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y 
+        dy = dout * self.x
+
+        return dx, dy
+
+
