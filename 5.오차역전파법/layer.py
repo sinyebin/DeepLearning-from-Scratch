aebd66a63faec8d511cf34cc1012f68543e80896
bin <dpqls0625@naver.com>
2023-06-19 21:14:42 +0900

    커밋

--- 5.오차역전파법/layer.py
+++ 5.오차역전파법/layer.py
@@ -34,3 +34,18 @@ class Relu:
         return dx
 
 
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = sigmoid(x)
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
